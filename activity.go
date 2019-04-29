90cb85a2fc3c9174097c80a9b0fac8d3e8355854
skothari-tibco <skothari@tibco.com>
2019-04-29 12:31:49 -0400

    initial

---
+++ activity.go
@@ -0,0 +1,159 @@
+package mongodb
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/TIBCOSoftware/flogo-lib/logger"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// ActivityLog is the default logger for the Log Activity
+var activityLog = logger.GetLogger("activity-flogo-mongodb")
+
+const (
+	methodGet     = "GET"
+	methodDelete  = "DELETE"
+	methodInsert  = "INSERT"
+	methodReplace = "REPLACE"
+	methodUpdate  = "UPDATE"
+
+	ivConnectionURI = "uri"
+	ivDbName        = "dbName"
+	ivCollection    = "collection"
+	ivMethod        = "method"
+
+	ivKeyName  = "keyName"
+	ivKeyValue = "keyValue"
+	ivData     = "data"
+
+	ovOutput = "output"
+	ovCount  = "count"
+)
+
+func init() {
+	activityLog.SetLogLevel(logger.InfoLevel)
+}
+
+/*
+Integration with MongoDb
+inputs: {uri, dbName, collection, method, [keyName, keyValue, value]}
+outputs: {output, count}
+*/
+type MongoDbActivity struct {
+	metadata *activity.Metadata
+}
+
+// NewActivity creates a new AppActivity
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	return &MongoDbActivity{metadata: metadata}
+}
+
+// Metadata returns the activity's metadata
+func (a *MongoDbActivity) Metadata() *activity.Metadata {
+	return a.metadata
+}
+
+// Eval implements api.Activity.Eval - MongoDb integration
+func (a *MongoDbActivity) Eval(ctx activity.Context) (done bool, err error) {
+
+	//mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]
+	connectionURI, _ := ctx.GetInput(ivConnectionURI).(string)
+	dbName, _ := ctx.GetInput(ivDbName).(string)
+	collectionName, _ := ctx.GetInput(ivCollection).(string)
+	method, _ := ctx.GetInput(ivMethod).(string)
+	keyName, _ := ctx.GetInput(ivKeyName).(string)
+	keyValue, _ := ctx.GetInput(ivKeyValue).(string)
+	value := ctx.GetInput(ivData)
+
+	//todo implement shared sessions
+	// client, err := mongo.NewClient(connectionURI)
+	/*
+		The above function was giving below error;
+		"data not inserted topology is closed"
+	*/
+	bCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	client, err := mongo.Connect(bCtx, options.Client().ApplyURI(connectionURI))
+
+	defer cancel()
+
+	defer client.Disconnect(context.Background())
+	if err != nil {
+		activityLog.Errorf("Connection error: %v", err)
+		return false, err
+	}
+
+	db := client.Database(dbName)
+	coll := db.Collection(collectionName)
+
+	switch strings.ToUpper(method) {
+	case methodGet:
+		result := coll.FindOne(bCtx, bson.M{keyName: keyValue})
+		val := make(map[string]interface{})
+		err := result.Decode(val)
+		if err != nil {
+			return false, err
+		}
+
+		activityLog.Debugf("Get Results $#v", result)
+
+		ctx.SetOutput(ovOutput, val)
+	case methodDelete:
+		result, err := coll.DeleteOne(bCtx, bson.M{keyName: keyValue}, nil)
+		if err != nil {
+			return false, err
+		}
+
+		activityLog.Debugf("Delete Results $#v", result)
+
+		ctx.SetOutput(ovCount, result.DeletedCount)
+	case methodInsert:
+		if value == nil && keyValue == "" {
+			// should we throw an error or warn?
+			activityLog.Warnf("Nothing to insert")
+			return true, nil
+		}
+
+		var result *mongo.InsertOneResult
+
+		if value != nil && keyValue == "" {
+			result, err = coll.InsertOne(bCtx, value)
+
+		} else {
+			result, err = coll.InsertOne(bCtx, bson.M{keyName: keyValue})
+		}
+
+		activityLog.Debugf("Insert Results $#v", result)
+
+		ctx.SetOutput(ovOutput, result.InsertedID)
+	case methodReplace:
+		result, err := coll.ReplaceOne(bCtx, bson.M{keyName: keyValue}, value)
+		if err != nil {
+			return false, err
+		}
+
+		activityLog.Debugf("Replace Results $#v", result)
+		ctx.SetOutput(ovOutput, result.UpsertedID)
+		ctx.SetOutput(ovCount, result.ModifiedCount)
+
+	case methodUpdate:
+		result, err := coll.UpdateOne(bCtx, bson.M{keyName: keyValue}, bson.M{"$set": value})
+		if err != nil {
+			return false, err
+		}
+
+		activityLog.Debugf("Update Results $#v", result)
+		ctx.SetOutput(ovOutput, result.UpsertedID)
+		ctx.SetOutput(ovCount, result.ModifiedCount)
+	default:
+		activityLog.Errorf("unsupported method '%s'", method)
+		return false, fmt.Errorf("unsupported method '%s'", method)
+	}
+
+	return true, nil
+}
---
+++ activity_test.go
@@ -0,0 +1,252 @@
+package mongodb
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/TIBCOSoftware/flogo-contrib/action/flow/test"
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/TIBCOSoftware/flogo-lib/logger"
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	TEST_URI  = "mongodb://localhost:27017"
+	TEST_DB   = "test"
+	TEST_COLL = "items"
+)
+
+var coll *mongo.Collection
+
+func init() {
+	//todo implement shared sessions
+	// client, err := mongo.NewClient(TEST_URI)
+
+	//To remove below  error:
+	// data not inserted topology is closed
+
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(TEST_URI))
+	if err != nil {
+		// warn and skip tests
+	}
+
+	db := client.Database(TEST_DB)
+	coll = db.Collection(TEST_COLL)
+}
+
+var activityMetadata *activity.Metadata
+
+func getActivityMetadata() *activity.Metadata {
+	if activityMetadata == nil {
+		jsonMetadataBytes, err := ioutil.ReadFile("activity.json")
+		if err != nil {
+			panic("No Json Metadata found for activity.json path")
+		}
+
+		activityMetadata = activity.NewMetadata(string(jsonMetadataBytes))
+	}
+
+	return activityMetadata
+}
+
+func randomString(n int) string {
+	rand.Seed(time.Now().UTC().UnixNano())
+	var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+
+func insert(dataVal interface{}) (interface{}, error) {
+
+	result, err := coll.InsertOne(
+		context.Background(),
+		dataVal,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	logger.Debug("Inserted: ", result.InsertedID)
+
+	return result.InsertedID, nil
+}
+
+func delete(id interface{}) {
+	oid := id.(primitive.ObjectID)
+	_, err := coll.DeleteOne(context.Background(), bson.M{"_id": oid})
+	if err != nil {
+		logger.Debugf("Error Deleting [%s] : %s", id, err.Error())
+		return
+	}
+	logger.Debug("Deleted", id)
+}
+
+// TestDelete
+func TestDelete(t *testing.T) {
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(getActivityMetadata())
+
+	name := randomString(5)
+	val := map[string]interface{}{"name": name, "value": "blah"}
+	_, err := insert(val)
+	if err != nil {
+		t.Error(err)
+		t.Fail()
+		return
+	}
+
+	tc.SetInput("uri", TEST_URI)
+	tc.SetInput("dbName", TEST_DB)
+	tc.SetInput("collection", TEST_COLL)
+	tc.SetInput("method", `DELETE`)
+
+	tc.SetInput(ivKeyName, "name")
+	tc.SetInput(ivKeyValue, name)
+
+	_, deleteErr := act.Eval(tc)
+	if deleteErr != nil {
+		t.Error("data not deleted")
+		t.Fail()
+	}
+}
+
+// TestInsert
+func TestInsert(t *testing.T) {
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(getActivityMetadata())
+
+	tc.SetInput("uri", TEST_URI)
+	tc.SetInput("dbName", TEST_DB)
+	tc.SetInput("collection", TEST_COLL)
+	tc.SetInput("method", `INSERT`)
+
+	name := randomString(5)
+	val := map[string]interface{}{"name": name, "value1": "foo", "value2": "foo2"}
+	tc.SetInput(ivData, val)
+	//tc.SetInput(ivKeyName, "key")
+	//tc.SetInput(ivKeyValue, "value")
+
+	_, insertErr := act.Eval(tc)
+	if insertErr != nil {
+		t.Error("data not inserted", insertErr)
+		t.Fail()
+	}
+	fmt.Println("Output ", reflect.TypeOf(tc.GetOutput(ovOutput)))
+
+	delete(tc.GetOutput(ovOutput))
+}
+
+// TestReplace
+func TestReplace(t *testing.T) {
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(getActivityMetadata())
+
+	name := randomString(5)
+	val := map[string]interface{}{"name": name, "value1": "foo", "value2": "foo2"}
+	id, err := insert(val)
+	if err != nil {
+		t.Error(err)
+		t.Fail()
+		return
+	}
+
+	tc.SetInput("uri", TEST_URI)
+	tc.SetInput("dbName", TEST_DB)
+	tc.SetInput("collection", TEST_COLL)
+	tc.SetInput("method", `REPLACE`)
+
+	val2 := map[string]interface{}{"name": name, "value1": "bar1", "value2": "bar2"}
+
+	tc.SetInput(ivKeyName, "name")
+	tc.SetInput(ivKeyValue, name)
+	tc.SetInput(ivData, val2)
+
+	_, replaceErr := act.Eval(tc)
+	if replaceErr != nil {
+		t.Error("data not replaced", replaceErr)
+		t.Fail()
+	}
+
+	delete(id)
+}
+
+// TestReplace
+func TestUpdate(t *testing.T) {
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(getActivityMetadata())
+
+	name := randomString(5)
+	val := map[string]interface{}{"name": name, "value1": "foo", "value2": "foo2"}
+	id, err := insert(val)
+	if err != nil {
+		t.Error(err)
+		t.Fail()
+		return
+	}
+
+	tc.SetInput("uri", TEST_URI)
+	tc.SetInput("dbName", TEST_DB)
+	tc.SetInput("collection", TEST_COLL)
+	tc.SetInput("method", `UPDATE`)
+
+	val2 := map[string]interface{}{"name": name, "value1": "bar1"}
+
+	tc.SetInput(ivKeyName, "name")
+	tc.SetInput(ivKeyValue, name)
+	tc.SetInput(ivData, val2)
+
+	_, updateErr := act.Eval(tc)
+	if updateErr != nil {
+		t.Error("update error", updateErr)
+		t.Fail()
+	}
+
+	delete(id)
+}
+
+// TestGet
+func TestGet(t *testing.T) {
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(getActivityMetadata())
+
+	name := randomString(5)
+	val := map[string]interface{}{"name": name, "value1": "foo", "value2": "foo2"}
+	id, err := insert(val)
+	if err != nil {
+		t.Error(err)
+		t.Fail()
+		return
+	}
+
+	tc.SetInput("uri", TEST_URI)
+	tc.SetInput("dbName", TEST_DB)
+	tc.SetInput("collection", TEST_COLL)
+	tc.SetInput("method", `GET`)
+
+	tc.SetInput(ivKeyName, "name")
+	tc.SetInput(ivKeyValue, name)
+
+	_, getErr := act.Eval(tc)
+	if getErr != nil {
+		t.Error("unable to get data", getErr)
+		t.Fail()
+	}
+
+	result := tc.GetOutput(ovOutput)
+	assert.NotNil(t, result)
+
+	delete(id)
+}
